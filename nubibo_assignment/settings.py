3056f85655be9b05777e9204b0535156522bf4e9
AayushDani <aayushdani1@gmail.com>
2020-07-27 18:26:13 +0530

    fix

--- nubibo_assignment/settings.py
+++ nubibo_assignment/settings.py
@@ -104,12 +104,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'staticfiles')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = "/static/"
 
